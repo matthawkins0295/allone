62553df1a1bb1db9fbdfe755bb9a2258a7a2b5c8
matthawkins0295 <166094951+matthawkins0295@users.noreply.github.com>
2024-05-28 15:19:42 -0700

    Update allone_dashboard.py
    Adding Stacked bar chart to labor

--- allone_dashboard.py
+++ allone_dashboard.py
@@ -115,8 +115,8 @@ def main():
 
             st.subheader(f"Labor %: {labor:.2f}")
         
-            aggregated_data2 = filtered_sales_data.groupby('Date')['Labor %'].mean().reset_index()
-            fig_labor = px.bar(aggregated_data2, x = 'Date', y= 'Labor %', title = 'Labor %')
+            aggregated_data2 = filtered_sales_data.groupby(['Date', 'Store'])['Labor %'].mean().reset_index()
+            fig_labor = px.bar(aggregated_data2, x='Date', y='Labor %', color='Store', title='Labor %', barmode='stack')
             st.plotly_chart(fig_labor)
 
         
